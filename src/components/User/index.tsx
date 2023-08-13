9e14c7fc2bb6fe0c3f1b470e073034c9303bff39
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2023-08-13 18:59:24 -0300

    initial style

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -3,18 +3,22 @@ import { Link } from "react-router-dom";
 import { UserProps } from "../../types/user";
 import { MdLocationPin } from "react-icons/md";
 
+import { Container, Local } from "./style";
+
 export default function User({ avatar_url, name, login, location }: UserProps) {
   return (
-    <div>
+    <Container>
       <Link to="/">
         <img src={avatar_url} alt="profile picture" />
       </Link>
       <p>{name}</p>
       <p>{login}</p>
-      <p>
+      {location && (
+      <Local>
         <MdLocationPin/>
         <span>{location}</span>
-      </p>
-    </div>
+      </Local>
+      )}
+    </Container>
   );
 }
--- src/components/User/style.tsx
+++ src/components/User/style.tsx
@@ -1,1 +1,36 @@
 import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #2b3566;
+  padding: 32px;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  img {
+    width: 140px;
+    height: 140px;
+    border: 4px solid #644aff;
+    border-radius: 50%;
+  }
+`
+
+const Local = styled.p`
+  display: flex;
+  align-items: center;
+
+  svg {
+    fill: #4ed8c7;
+    font-size: 24px; 
+  }
+
+  span {
+    color: #9da5d1;
+    font-weight: 700;
+  }
+`
+
+export { Container, Local }
