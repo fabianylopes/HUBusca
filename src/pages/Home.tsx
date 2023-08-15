16c9ee241bdfb25dc59ac9cda8209dafa56cfece
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2023-08-15 20:26:58 -0300

    add error msg when user is not found

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,16 +4,28 @@ import axios from "axios";
 import Search from "../components/Search";
 import { UserProps } from "../types/user";
 import User from "../components/User";
+import Error from "../components/Error";
 
 export default function Home() {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
   
-  async function loadUser(userName: string){
-    const res = await axios.get(`https://api.github.com/users/${userName}`);
-    const data = await res.data;
-
-    const { avatar_url, name, login, location } = data;
-
+  function loadUser(userName: string){
+  
+   const res = axios.get(`https://api.github.com/users/${userName}`)
+    .then((response) => setUser(response.data))
+    .catch(handleError);
+   
+   function handleError(){
+     setUser(null);
+     setError(true);
+     return;
+    }
+    
+    setError(false);
+  
+    const { avatar_url, name, login, location } = user;
+    
     const userData: UserProps = {
       avatar_url,
       name,
@@ -29,6 +41,7 @@ export default function Home() {
     <div>
       <Search loadUser={loadUser}/>
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   )
 }
